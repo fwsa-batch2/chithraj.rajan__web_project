db9367ca86b71ed62ee2c144a17b69b4414fc666
Chithraj <chithraj23102003@gmail.com>
2022-01-18 17:23:42 +0530

    added new design for home page in new page

--- assets/js/sthome.js
+++ assets/js/sthome.js
@@ -1,3 +1,14 @@
-function goToCategory(){
-    window.location.href = "./../pages/storycontent.html" 
+function displayCategory(value){
+    switch(value){
+    case "stories":
+        window.location.href = "storyscotent.html"
+    case "poems":
+    window.location.href = "storyscotent.html"
+    case "videos":
+    window.location.href = "storyscotent.html"
+    case "comics":
+    window.location.href = "storyscotent.html"
+
+
+    }
 }
