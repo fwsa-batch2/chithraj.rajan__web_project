fa424f659c969dc6424011b3b0bff556d03655ed
Chithraj <chithraj23102003@gmail.com>
2022-01-19 17:05:04 +0530

    modified ny new page

--- assets/js/sthome.js
+++ assets/js/sthome.js
@@ -1,14 +1,19 @@
 function displayCategory(value){
     switch(value){
     case "stories":
-        window.location.href = "storyscotent.html"
+    window.location.href = "storyscotent.html";
+    break;
     case "poems":
-    window.location.href = "storyscotent.html"
+    window.location.href = "storyscotent.html";
+    break;
     case "videos":
-    window.location.href = "storyscotent.html"
+    window.location.href = "storyscotent.html";
+    break;
     case "comics":
-    window.location.href = "storyscotent.html"
+    window.location.href = "storyscotent.html";
+    break;
 
 
     }
+
 }
