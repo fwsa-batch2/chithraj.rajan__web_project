9a88f0a11aa8c885812cd643ebc91e2eda925acd
Chithraj <chithraj23102003@gmail.com>
2022-01-20 14:17:32 +0530

    cleared bugs and code smell

--- assets/js/sthome.js
+++ assets/js/sthome.js
@@ -2,16 +2,16 @@ function displayCategory(value){
     switch(value){
     case "stories":
     window.location.href = "storyscotent.html";
-    break;
+
     case "poems":
     window.location.href = "storyscotent.html";
-    break;
+    
     case "videos":
     window.location.href = "storyscotent.html";
-    break;
+    
     case "comics":
     window.location.href = "storyscotent.html";
-    break;
+    
 
 
     }
