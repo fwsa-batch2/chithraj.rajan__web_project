dba7509d77ce6682353e9ada0eb79208c3a7251a
Chithraj <chithraj23102003@gmail.com>
2022-01-21 16:42:46 +0530

    ---

---
+++ assets/js/storycontent.js
@@ -0,0 +1,21 @@
+function goMoral(){
+    window.location.href="moral stories.html"
+}
+function goPancha(){
+    window.location.href="panchatantra stories.html"
+}
+
+function displayCategory(value){
+        switch(value){
+        case "stories":
+            window.location.href = "storyscotent.html"
+        case "poems":
+        window.location.href = "storyscotent.html"
+        case "videos":
+        window.location.href = "storyscotent.html"
+        case "comics":
+        window.location.href = "storyscotent.html"
+
+
+        }
+    }
