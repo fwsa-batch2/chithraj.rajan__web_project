5b877cf1d44ac0d93358f78d8ce72bcca0aa5631
Chithraj <chithraj23102003@gmail.com>
2022-01-21 15:21:34 +0530

    cleared some errors

--- assets/js/login.js
+++ assets/js/login.js
@@ -33,7 +33,7 @@ function userInput(event) {
 
     if (password === true && emailId === true && getUserName === true ) {
         console.log("hello");
-        window.location.href = "/pages/imageslider.html";
+        window.location.href = "/pages/kiddyHome.html";
         
     }
     
